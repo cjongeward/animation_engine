38ed93995b048126f49ef2dc7ebc1fcb3576b825
cj <chris.jongeward@gmail.com>
2019-03-03 22:42:15 -0800

    named the shading multipliers

--- 3-Specular_Lighting/tracer/main.cpp
+++ 3-Specular_Lighting/tracer/main.cpp
@@ -31,13 +31,16 @@ public:
             final_color = shape.color;
           }
           else {
+            final_color = final_color + shape.color * AMBIENT_LIGHT_INTENSITY;
             for (auto& secondary_shape : shapes) {
               if (secondary_shape.bIsLightSource) {
                 vec lightDir = secondary_shape.pos - reflected->reflection.pos;
                 lightDir.normalize();
                 float diffuse_light_intensity = std::max(0.f, reflected->norm.dot(lightDir));
                 float specular_light_intensity = std::powf(std::max(0.f, (-reflect(-lightDir, reflected->norm)).dot(primary.dir)), 50.f);
-                final_color = final_color + shape.color * 0.5f * (AMBIENT_LIGHT_INTENSITY + diffuse_light_intensity) + secondary_shape.color * 0.8f * specular_light_intensity;
+                constexpr float diffuse_mult = 0.5f;
+                constexpr float spec_mult = 0.8f;
+                final_color = final_color + shape.color * diffuse_mult * diffuse_light_intensity + secondary_shape.color * spec_mult * specular_light_intensity;
               }
             }
           }
