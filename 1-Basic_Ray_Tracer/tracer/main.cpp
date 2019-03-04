16fca2fa666daa96d9fc90c31aa30761be7dc1b4
cjongeward <chris.jongeward@gmail.com>
2019-03-04 09:20:33 -0800

    Fixed formatting in main

--- 1-Basic_Ray_Tracer/tracer/main.cpp
+++ 1-Basic_Ray_Tracer/tracer/main.cpp
@@ -57,23 +57,23 @@ int main() {
   Screen screen{ vec{-4.f, -3.f, 0.f}, 8.f, 6.f };
   Sphere sphere{ vec{0.f, 0.f, -4.f}, RED, 1.f };
 
-	unsigned int* img = new unsigned int[RESX * RESY];
-	for (int row = 0; row < RESY; ++row) {
-		for (int col = 0; col < RESX; ++col) {
-			float px = screen.pos.x + screen.sizex * static_cast<float>(col) / RESX;
-			float py = screen.pos.y + screen.sizey * static_cast<float>(row) / RESY;
+  unsigned int* img = new unsigned int[RESX * RESY];
+  for (int row = 0; row < RESY; ++row) {
+    for (int col = 0; col < RESX; ++col) {
+      float px = screen.pos.x + screen.sizex * static_cast<float>(col) / RESX;
+      float py = screen.pos.y + screen.sizey * static_cast<float>(row) / RESY;
       Ray ray{ vec{px, py, 0.f}, vec{0.f, 0.f, -1.f} };
       unsigned color = BLACK;
       if (intersects(sphere, ray)) {
         color = sphere.color;
       }
       img[RESX*(RESY - row - 1) + col] = color;
-		}
-	}
+    }
+  }
 
   bool result = intarray2bmp::intarray2bmp("image.bmp", img, RESY, RESX);
 
-	delete[] img;
+  delete[] img;
 
   system("start  C:\\WINDOWS\\system32\\mspaint.exe C:\\Users\\caj34\\Documents\\repos\\animation_engine\\1-Basic_Ray_Tracer\\tracer\\image.bmp");
 }
