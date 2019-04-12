5e48b7ea765653792e67527994babd0acfd3b6ae
cj <chris.jongeward@gmail.com>
2019-04-11 22:51:54 -0700

    moved nearest collision detection to a function

--- 08-Shadows_Antialiasing/tracer/RayTracer.cpp
+++ 08-Shadows_Antialiasing/tracer/RayTracer.cpp
@@ -14,18 +14,13 @@ Color RayTracer::trace(const std::vector<std::unique_ptr<Shape>>& shapes) const 
   return trace(shapes, primary_ray, 0, nullptr);
 }
 
-Color RayTracer::trace(const std::vector<std::unique_ptr<Shape>>& shapes, const Ray& incidentRay, int depth, const Shape* curShape) const {
-  if (depth >= max_depth) { // recursive base case
-    return BLACK;
-  }
-
-  // find the nearest hit point
+auto findNearestHitPoint(const std::vector<std::unique_ptr<Shape>>& shapes, const Ray& incidentRay, const Shape* pIgnoreShape) {
   const Shape* pNearestShape = nullptr;
   std::optional<ReflectionData> nearestReflection = std::nullopt;
   for (const auto& shape_up : shapes) {
     Shape* pShape = shape_up.get();
     Shape& shape = *pShape;
-    if (&shape != curShape) {
+    if (&shape != pIgnoreShape) {
       auto reflected = shape.intersects_with(incidentRay);
       if (reflected.has_value()) {
         if (!nearestReflection.has_value()) {
@@ -39,6 +34,15 @@ Color RayTracer::trace(const std::vector<std::unique_ptr<Shape>>& shapes, const 
       }
     }
   }
+  return std::make_pair(pNearestShape, nearestReflection);
+}
+
+Color RayTracer::trace(const std::vector<std::unique_ptr<Shape>>& shapes, const Ray& incidentRay, int depth, const Shape* curShape) const {
+  if (depth >= max_depth) { // recursive base case
+    return BLACK;
+  }
+
+  auto [pNearestShape, nearestReflection] = findNearestHitPoint(shapes, incidentRay, curShape);
 
   // light it up
   Color final_color = BLACK;
@@ -83,38 +87,6 @@ Color RayTracer::trace(const std::vector<std::unique_ptr<Shape>>& shapes, const 
         }
       }
     }
-
-    //for (const auto& shape_up : shapes) {
-    //  Shape* pShape = shape_up.get();
-    //  if (pShape != pNearestShape && pShape->properties.intensity > 0.f) {
-    //    vec lightDir = pShape->pos - pNearestShape->pos;
-    //    float mag2 = lightDir.mag2();
-    //    lightDir.normalize();
-    //    bool blocked = false;
-    //    //for (const auto& shape_up2 : shapes) {
-    //    //  Shape* pShape2 = shape_up2.get();
-    //    //  if (auto ref = pShape2->intersects_with(Ray{ pShape->pos, lightDir }); ref.has_value()) {
-    //    //    vec shapeDir = ref->reflection.pos - pNearestShape->pos;
-    //    //    float shapeDirMag2 = shapeDir.mag2();
-    //    //    if (shapeDirMag2 < mag2) {
-    //    //      blocked = true;
-    //    //    }
-    //    //  }
-    //    //}
-
-    //    if (!blocked) {
-    //      float diffuse_light_intensity = std::max(0.f, nearestReflection->norm.dot(lightDir)) * pShape->properties.intensity;
-    //      const auto r = -reflect(-lightDir, nearestReflection->norm);
-    //      const auto rdotprim = std::max(0.f, r.dot(primary_ray.dir));
-    //      float specular_light_intensity = myPow(rdotprim, pNearestShape->properties.specular_exp) * pShape->properties.intensity;
-    //      final_color += pNearestShape->properties.color * pNearestShape->properties.diffuse_factor * diffuse_light_intensity +
-    //        pShape->properties.color * pNearestShape->properties.specular_factor * specular_light_intensity;
-
-    //    }
-
-    //  }
-    //}
-
   }
   return final_color;
 }
